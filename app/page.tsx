f29e34ee20976018ce93f7b3f1a44e29de61c8d3
Eddie Zhuang <zhuang.eddie@gmail.com>
2025-05-18 15:46:00 -0400

    Add logo to header

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import {
   NavigationMenu,
   NavigationMenuList,
 } from "~/components/ui/NavigationMenu";
+import Link from "next/link";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { Button } from "~/components/ui/Button";
 import { Canvas } from "@/components/Canvas";
@@ -15,18 +17,34 @@ export default function HomePage() {
   return (
     <CanvasProvider>
       <NavigationMenu>
-        <NavigationMenuList>
-          <Button
-            variant="outline"
-            size="md"
-            onClick={() => router.push("/login")}
-          >
-            Log in
-          </Button>
-          <Button variant="primary" size="md">
-            Sign up
-          </Button>
-        </NavigationMenuList>
+        <div className="flex items-center justify-between h-full w-full max-w-[1265px]">
+          <NavigationMenuList>
+            <Link href="/">
+              <Image
+                src="/light/wordmark.svg"
+                width={772}
+                height={200}
+                alt="Picture of the author"
+                style={{
+                  width: "auto",
+                  height: "28px",
+                }}
+              />
+            </Link>
+          </NavigationMenuList>
+          <NavigationMenuList>
+            <Button
+              variant="outline"
+              size="md"
+              onClick={() => router.push("/login")}
+            >
+              Log in
+            </Button>
+            <Button variant="primary" size="md">
+              Sign up
+            </Button>
+          </NavigationMenuList>
+        </div>
       </NavigationMenu>
 
       <main className="flex flex-col items-center justify-center w-screen h-screen bg-gray-200">
