9d7ff83318fe8300fe2dddb3ac7d86e09dbd9400
Eddie Zhuang <zhuang.eddie@gmail.com>
2025-05-26 16:28:04 -0400

    Refactor matcher comments in middleware configuration for clarity

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,11 +4,9 @@ export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    // Standard matcher for pages, excluding static files and _next internals
-    // Next.js will prepend the basePath to this pattern automatically for runtime.
-    "/((?!_next|[^?]*\\\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Standard matcher for API routes
-    // Next.js will prepend the basePath to this pattern automatically for runtime.
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
     "/(api|trpc)(.*)",
   ],
 };
